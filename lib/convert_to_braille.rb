6ddcdd9d3f487cd19ac933f31614c2c53a6b99f0
Karan Mehta <karanm645@gmail.com>
2021-09-26 19:28:33 -0500

    Create convert method and change method

--- lib/convert_to_braille.rb
+++ lib/convert_to_braille.rb
@@ -1,2 +1,21 @@
+
 class ConvertToBraille
-end 
+
+  def initialize
+
+  end
+
+  def self.convert(input_file)
+    array = []
+    input_file.chars.find_all do |character|
+      if character != '\n'
+        array << change(character)
+      end
+    end
+    array.compact.flatten
+  end
+
+  def self.change(letter)
+    Library.find_value(letter)
+  end
+end
