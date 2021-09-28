fe97d4e0561ea0e3b4d36497565b1129c6cd3924
Karan Mehta <karanm645@gmail.com>
2021-09-27 23:37:44 -0500

    udpate english converter

--- lib/convert_to_english.rb
+++ lib/convert_to_english.rb
@@ -1,8 +1,22 @@
 class ConvertToEnglish
+  attr_reader :array
 
-  def self.convert(string)
-    array = []
-    string
+  def initialize
+    # @lb = Library.new
+    @array = []
+  end
+
+  def convert(string)
+    string.chars.each do |str|
+      if str != '\n'
+        array << change(letter)
+    require "pry"; binding.pry
+      end
+    end
+  end
+
+  def change(letter)
+    @lb.find_letter_value(letter)
     require "pry"; binding.pry
   end
 end
