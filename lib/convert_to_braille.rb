e1238a09d06305f3d967df8fb696294a03adbe01
Karan Mehta <karanm645@gmail.com>
2021-09-27 23:37:26 -0500

    Update braille converter

--- lib/convert_to_braille.rb
+++ lib/convert_to_braille.rb
@@ -5,41 +5,35 @@ class ConvertToBraille
   end
 
   def convert(input_file)
+    #created an empty array to push newly converted braille
     array = []
-    split_string(input_file).find_all do |character|
+    #called method split_string(message.txt) and find all characters
+    split_string(input_file).each do |character|
       if character != '\n'
+        #this converts the letters to braille and pushes it into the array
         array << change(character)
       end
     end
+    #i now have removed the nils and transposed to get the top middle and bottom arrays split accordingly
     x = array.compact.transpose
+    #to get the format correct, i'm mapping over x and removing quotations
     x.map { | row | row.join(' ')}.join("\n")
       x.map do |row|
         rj = row.join(' ')
-
+        #i'm also breaking the rows off so the values return in different rows
       end.join("\n")
   end
 
+  # my first attempt to break down the above method (single responsibility)
   def split_string(input_file)
     input_file.chars
   end
 
   def change(letter)
+    #this dives into the library and find the braille value of the associated letter
     @lb.find_value(letter)
   end
 end
-  # def convert_to_english(input_file)
-  #   array = []
-  #   input_file
-  #   require "pry"; binding.pry
-  # end
-  # def self.check_for_less_than_eighty_characters(rj)
-  #
-  #   if rj.size > 80
-  #
-  #   end
-  #   #braille.txt (output/ARGV[1], check if character count > 80 \n -- keep printing)
-  #   # else character.size <= 80 then return converted row
-  # end
 
 # if rj.size > 80
 #    x << ["a", "b", "c"]
